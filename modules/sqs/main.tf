2c5461d4aaf48b07daec906c7fac051840fe9e64
dossancto <dossantossilvalucas963@gmail.com>
2024-07-05 21:58:56 -0300

    change to fifo queue

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,9 +1,12 @@
 resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terraform-example-queue"
+  name                      = "terraform-example-queue.fifo"
   delay_seconds             = 1
+  fifo_queue                = true
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+
+  content_based_deduplication = true
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
